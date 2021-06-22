623808dcdfa9523a77203d4a8ce1f50a00bfcf3f
peterkhang <mgkaki@bory.io>
2021-06-22 19:09:30 +0900

    [kepha] add firebase deploy

--- androidReal/copy.bat
+++
@@ -1,2 +0,0 @@
-del app\src\main\assets\public\*
-copy ..\android\app\src\main\assets\public\* app\src\main\assets\public\
--- androidReal/copy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/rm -rf app/src/main/assets/public/*
-cp -r ../android/app/src/main/assets/public/* app/src/main/assets/public/
